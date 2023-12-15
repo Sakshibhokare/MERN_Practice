e534c11c3068604ba6656ae965d6b4eb77591769
SakshiBhokare <bhokaresakshi93@gmail.com>
2023-12-15 14:34:25 +0530

    Basics of javaScript

---
+++ JavaScript/Practice_1.js
@@ -0,0 +1,33 @@
+// introduction to JS 
+// variable
+// premitive and non premitive 
+// premitives are string, number, boolean,undefined, null 
+// BigInt, Symbol(), 
+// non-premitive: object 
+
+var name= "Sakshi"
+console.log(name);
+
+var a=10;
+var b="10";
+// true loose equality
+console.log(a==b);
+
+// false strict equality
+console.log(a===b);
+
+// Type conversion 
+let numb='10'
+parseInt(a);
+
+// automatically conversion/ cohensive type conversion 
+console.log(10+"10"); //1010 + is use to add two string so it works as a strings
+
+
+console.log(10-"10");//0 works as a number because - 
+
+console.log("Sakshi"-10);//NaN not a number
+
+let a='10';
+// also we can use +a to convert in int value 
+
