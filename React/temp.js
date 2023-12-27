6f159e0f9ef9a43a2072c4097f46dda38318b729
SakshiBhokare <bhokaresakshi93@gmail.com>
2023-12-27 21:52:49 +0530

    react practice

---
+++ React/temp.js
@@ -0,0 +1,7 @@
+// module : a webPage 
+//library: no of modules in it 
+//framework: no of libraries 
+// it will provide a complete development tool to get that project done
+// react is an library because you have to borrow many features, which help to create front end in more optimized manner
+// React.dev: documentation
+// vite is more popular now 
