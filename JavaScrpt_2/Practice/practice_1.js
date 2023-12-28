64a6b2697842581ec5b72a58c62c0e9104e68f0b
SakshiBhokare <bhokaresakshi93@gmail.com>
2023-12-28 16:59:17 +0530

    codehelp js series

--- JavaScrpt_2/Practice/practice_1.js
+++ JavaScrpt_2/Practice/practice_1.js
@@ -69,7 +69,7 @@ function table(numb){
     }
 }
 table(6)
-
+console.log("----------------------------------------------------------------")
 // 8. Write a program that takes a number and calculates the sum of all numbers from 1 to that number
 function total(a){
     let ttl=0;
@@ -80,3 +80,41 @@ function total(a){
 }
 
 total(19)
+console.log("------------------------------------------------------")
+//9. Write a program that takes a string and prints out the number of vowels in the string.
+function countVowels(strng){
+    const vowels=["a", "e", "i", "o", "u"];
+    let count=0;
+
+    for(let i=0; i<strng.length; i++){
+        if(vowels.includes(strng[i].toLowerCase())){
+            count++;
+        }
+    }
+
+    return count;
+}
+
+console.log(countVowels("Sakshi Bhokare"))
+
+console.log("-----------------------------------------------------")
+// 10. Write a function that takes two arrays of integers as arguments and returns an array of the common elements in both arrays, without any duplicates. The returned array should be sorted in ascending order.
+// For example, given the arrays [1, 2, 3, 4, 5] and [3, 4, 5, 6, 7], the function should return [3, 4, 5]. 
+
+function commonElmnt(arr1, arr2){
+    let commomElements=[];
+ for(let i=0; i<arr1.length; i++){
+    if(arr2.includes(arr1[i])){
+        if(!commomElements.includes(arr1[i])){
+            commomElements.push(arr1[i]);
+        }
+    }
+ }
+ commomElements.sort((a,b)=>a-b);
+ return commomElements;
+}
+
+let arr1=[4,5,6,7,8,8,9]
+let arr2=[5,5,6,7,7]
+let commomElements=commonElmnt(arr1, arr2)
+console.log(commomElements)
