785f57885c6240b67f12b75e3f8f175a41bddbe4
SakshiBhokare <bhokaresakshi93@gmail.com>
2023-12-28 15:38:09 +0530

    calculator app is working

---
+++ JavaScript/CalculatorApp/index.js
@@ -0,0 +1,30 @@
+let input = document.getElementById('inputBox');
+let buttons= document.querySelectorAll('button');
+let reset=document.getElementsByClassName('reset')[0];
+let string="";
+
+let arr= Array.from(buttons);
+
+arr.forEach(button=>{
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML=='='){
+            string=eval(string);
+            input.value=string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string=string.substring(0, string.length-1);
+            input.value=string;
+        }
+        else{
+            string+=e.target.innerHTML;
+            input.value=string;
+        }
+
+    })
+})
+
+reset.addEventListener('click', ()=>{
+    window.location.reload();
+})
+
+
