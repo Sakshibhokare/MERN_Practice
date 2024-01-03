5929fbf0fdc240cb269447a48fd74e1886d5871e
SakshiBhokare <bhokaresakshi93@gmail.com>
2024-01-03 14:32:21 +0530

    promises complete

--- Codehelp/Dom Manipulation/dom4.js
+++ Codehelp/Dom Manipulation/dom4.js
@@ -71,6 +71,86 @@ let output = waada1.then(()=>{
     })
     return waada2; //returning a promise 
 })
-
+//output is also a promise 
 output.then((value)=>console.log(value)) //value will be output of waada2
 
+
+//******** async await *******
+// ayncs function works with promises and returns a promise always 
+// if we want p2 execute after p1 then we can use await keyword to stop the execution of p2 before p1 
+
+async function demo(){
+    return 4012003;
+}
+console.log(demo)
+// output: [AsyncFunction: demo] 
+
+async function utility(){
+    let delhiMausam= new Promise(function(resolve, reject){
+        setTimeout(()=>{
+            console.log("Delhi me bhot garmi hai");
+        }, 10000);
+    });
+
+    let hydMausam= new Promise(function(resolve, reject){
+        setTimeout(()=>{
+            console.log("Hyderabad is cool")
+        }, 5000)
+        
+    });
+
+    let dM= await delhiMausam;
+    let hdM= await hydMausam;
+    
+
+    return [dM, hdM];
+}
+
+console.log(utility())
+
+
+//*********** fetch api ***********
+//get call
+async function util(){
+    let content = await fetch('https://jsonplaceholder.typicode.com/todos/4');
+    let output=await content.json();
+    console.log(output)
+}
+
+util()
+
+// to fetch api will give two parameter 
+// fetch('url', [options])  in option we can add authentication key or something which can help in validation  
+
+//post call using fetch 🤩🤩🤩☆*: .｡. o(≧▽≦)o .｡.:*☆
+// fetch('url', optionWalaOBJ)
+
+async function helper(){
+
+
+let options= {
+    method:'POST',
+    body:JSON.stringify({  //json ko string me convert
+        title:'foo',
+        body:'bar',
+        userId: 1,
+        weight:60,
+        title:"YoYo"
+    }),
+    headers:{
+        'Content-type':'application/json; charset=UTF-8',
+    },
+
+};
+
+let content=await fetch('https://jsonplaceholder.typicode.com/posts', options);
+let response=content.json();
+return response;
+}
+
+async function utilities(){
+    let ans =await helper();
+    console.log(ans)
+}
+
+utilities()
