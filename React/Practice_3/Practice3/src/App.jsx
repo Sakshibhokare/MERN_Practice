3fff3ecb5b127d17d447d3dfc413ac1cdff07ee4
SakshiBhokare <bhokaresakshi93@gmail.com>
2024-01-11 15:45:11 +0530

    states

--- React/Practice_3/Practice3/src/App.jsx
+++ React/Practice_3/Practice3/src/App.jsx
@@ -3,6 +3,30 @@ import Footer from "./Footer"
 import Card from "./PropsCard/Card"
 
 function App() {
+  // 5th maps and arrays 
+//we can map array elements to
+  let arr=[1, 3, 5, 12];
+  arr.map((item)=>{
+    return console.log(item)
+  })
+
+  let arraOfarrays = [[3,4,5], [23, 34, 54]]
+  arraOfarrays.map((item)=>{
+    return item.map((subItem)=>{
+      console.log(subItem)
+    })
+  })
+
+  let productArr=[
+    {id:1001, title:"shoes", price:10, stock:20},
+    {id:1002, title:"hoodies", price:20, stock:40},
+    {id:1003, title:"jeans", price:50, stock:40},
+    {id:1004, title:"tops", price:10, stock:12}
+  ];
+
+  productArr.map((item)=>{
+    return console.log(item.id)
+  })
 
   return (
     <>
@@ -15,10 +39,25 @@ function App() {
 
 
    {/* 4th Props */}
-   <Card title={'jeans'} price={'20$'}></Card>
-   <Card title={'shoes'} price={'19.9$'}></Card>
-   <Card title={'hoodies'} price={'19.9$'}></Card>
-   
+   <Card  price={20}></Card>
+   <Card title={'dress'} price={'19.9'}></Card> 
+   <Card title={'T-shirt'} price={19.9}></Card>
+
+    
+
+    {/* 5th map and arrays  */}
+    {/* for objs we need to use () instead of {} */}
+    {
+      productArr.map((product)=>(
+        <Card key={product.id} title={product.title} price={product.price} stock={product.stock}></Card>
+      ))
+    }
+  
+  {/* 6th states  
+  when we want to decrement the values but it can not desplay thats why we should use state variable
+  for that we will add hook which is useState so we can update and replace the value*/}
+
+
    </>
   )
 }
--- React/Practice_3/Practice3/src/PropsCard/Card.jsx
+++ React/Practice_3/Practice3/src/PropsCard/Card.jsx
@@ -1,25 +1,58 @@
 import React from "react";
 import './Card.css';
-import PropTypes from 'prop-types'
+import propTypes from 'prop-types'
+import { useState } from "react";
 
 
 //props: in the  funtction we will add props or parameters so we can use it as a template 
 // we can add the props values in our app.js file 
-function Card(props){
-  
+function Card({title, price, stock}){
+    //reset the stock value
+    let prevStock=stock;
+
+    //decrese the stock value
+    let [stockCount, setStockCount]=useState(stock);
+
+
+  function decrementStock(){
+    setStockCount(--stockCount);
+    console.log(stockCount)
+  }
+
+  function resetStock(){
+    setStockCount(prevStock)
+  }
+
+
     return(
         <div className="Card">
-          <h1>{props.title}</h1>
-          <p className="price">{props.price}</p>
-          <p><button>Add to Cart</button></p>
+          <h1>{title}</h1>
+          <p className="price">{price}</p>
+          <p className="stock">stock {stockCount}</p>
+          <p><button onClick={decrementStock}>Add to Cart</button></p>
+          {/* <p onClick={resetStock}>Reset the stock</p> */}
         </div>
     )
 }
+
+//to define the type of a parameter will use a dependnace called prop-types
+//so if we perform addition of numbers then we can do it by understanding the data type of the parameter 
+// warning will come we have not followed the datatype 
 Card.propTypes={
     title: propTypes.string,
     price: propTypes.number
 }
-//to define the type of a parameter will use a dependnace called prop-types
-//so if we perform addition of numbers then we can do it by understanding the data type of the parameter 
+
+// default props 
+Card.defaultProps={
+    title:'Cloths'
+}
+
+//required props 
+// Card.requiredProps={
+//     title
+// }
+
+
 
 export default Card 
