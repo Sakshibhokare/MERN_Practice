c8f4c8833c88504a309a8a6c9d0d5e838f2fbf22
SakshiBhokare <bhokaresakshi93@gmail.com>
2024-02-21 22:58:05 +0530

    Basics of Database

---
+++ Backend/first.js
@@ -0,0 +1,6 @@
+// Different js engines 
+// ex . v8 
+// every browser has a js engine that 
+// chrome has v8 engine... 
+// node works as a engine, so we are not dependant on browser to run js code 
+// npm is node package manager : it is responsible to in handling diffrent packages and dependancies 
