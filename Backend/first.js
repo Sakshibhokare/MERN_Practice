3a998d8e6f6f626ccb8a5ba3fabefecf9a14463a
SakshiBhokare <bhokaresakshi93@gmail.com>
2024-02-21 23:06:46 +0530

    can ignore this file

--- Backend/first.js
+++ Backend/first.js
@@ -4,3 +4,8 @@
 // chrome has v8 engine... 
 // node works as a engine, so we are not dependant on browser to run js code 
 // npm is node package manager : it is responsible to in handling diffrent packages and dependancies 
+// express we use to work with node js 
+// ES6 : es6 syntax are very important 
+
+// for js revision: How much js do you need to know to use nodejs check once /
+// promises, async functions need to check before second class 
