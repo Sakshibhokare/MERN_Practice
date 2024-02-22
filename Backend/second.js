01250525f5e4ebdb4ba58d122bea79a8d9ee8b4c
SakshiBhokare <bhokaresakshi93@gmail.com>
2024-02-22 22:52:47 +0530

    Ignore this file

---
+++ Backend/second.js
@@ -0,0 +1,6 @@
+// web frameworks: react, nextjs, express 
+// MVC model 
+// Model View Controller 
+// 1) whever the user makes the request it goes to the controller 
+// 2) controller decides and give data to view 
+// routes is a specific path which is taken by the controller 
