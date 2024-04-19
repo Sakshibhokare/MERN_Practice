fead31346d45c8e2951c96c10dac655356a51d68
Sakshi Bhokare <bhokaresakshi93@gmail.com>
2024-04-19 18:17:25 +0530

    fetch and Axios

--- React/Practice-7/APIsOnReact/src/App.jsx
+++ React/Practice-7/APIsOnReact/src/App.jsx
@@ -1,9 +1,35 @@
-
+import { useState } from 'react'
+import React from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  
 
+  let [fact, setFact] = useState('Loading...');
+
+  const fetchFacts = ()=>{
+    fetch('https://catfact.ninja/fact').then((res)=>{
+      return res.json()
+    }).then((data)=>{
+      console.log(data.fact);
+      setFact( data.fact);
+    })
+    .catch((err)=>{
+      alert('something went wrong')
+    })
+  }
+
+
+  //Axios is a third party package which we can use to fetch our data
+  const fetchFactsViaAxios=()=>{
+    axios.get('https://catfact.ninja/fact').then((res)=>{
+      setFact(res.data.fact);
+    }).catch((err)=>{
+      alert('something went wrong');
+    })
+  }
+  
+  
   return (
     <>
     {/* Fetch and Axios
@@ -11,9 +37,12 @@ function App() {
 
     <div className='app'>
       <div className='card'>
-        <h1 className='heading'>Facts received from api will appear here</h1>
+        <h1 className='heading'>{fact}</h1>
         <button className='button'>
-          <span>Show more</span>
+          <div onClick={fetchFacts}>Show more</div>
+        </button>
+        <button>
+        <div onClick={fetchFactsViaAxios}>Show moreeee</div>
         </button>
       </div>
       </div>     
